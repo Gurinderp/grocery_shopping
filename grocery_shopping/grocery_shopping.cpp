e40b365665f801b56f9413dd19c7ae0d3f7643a4
rednirug <sgurinderpreet@gmail.com>
2021-02-15 01:34:36 -0800

    Basic function layout, greet, display, process, checkout, and exit

--- grocery_shopping/grocery_shopping.cpp
+++ grocery_shopping/grocery_shopping.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+void greetCustomer();
+void displayGrocery();
+void processGrocery();
+void checkoutCustomer();
+void exitStore();
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    char customerName;
+    int apples;
+
+    greetCustomer();
+    displayGrocery();
+    processGrocery();
+    checkoutCustomer();
+    exitStore();
 }
+
+void greetCustomer() {
+    std::cout << "Welcome to Gurinder's Gurocer!" << std::endl;
+};
+
+void displayGrocery() {
+    std::cout << "Display Grocery available and amount here" << std::endl;
+};
+
+void processGrocery() {
+    std::cout << "Should take the inventory and remove what the customer took." << std::endl;
+}
+
+void checkoutCustomer() {
+    std::cout << "Need to create balance that is deducted" << std::endl;
+};
+
+void exitStore() {
+    std::cout << "Thank you for shopping with us! Have a nice day." << std::endl;
+};
