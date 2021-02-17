8ebad8283d9b9c002df84870d3b2c928fbdba733
rednirug <sgurinderpreet@gmail.com>
2021-02-16 19:01:22 -0800

    created login and register functions, added to signin function, added more grocery variables

--- grocery_shopping/grocery_shopping.cpp
+++ grocery_shopping/grocery_shopping.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-void greetCustomer();
+void signIn(std::string& customerName, int& sign_in_selection);
+void customerLogin(std::string& customerName);
+void customerRegister(std::string& customerName);
+void greetCustomer(std::string customerName);
 void displayGrocery();
 void processGrocery();
 void checkoutCustomer();
@@ -8,18 +11,54 @@ void exitStore();
 
 int main()
 {
-    char customerName;
+    std::string customerName = "Guest";
+    int sign_in_selection;
     int apples;
+    int bananas;
+    int oranges;
+    int hershey;
+    int twix;
+    int taffy;
+    int jolly_rancher;
+    int chicken;
+    int steak;
+    int pork_chop;
 
-    greetCustomer();
+    signIn(customerName, sign_in_selection);
+    greetCustomer(customerName);
     displayGrocery();
     processGrocery();
     checkoutCustomer();
     exitStore();
 }
 
-void greetCustomer() {
-    std::cout << "Welcome to Gurinder's Gurocer!" << std::endl;
+void signIn(std::string& customerName, int& sign_in_selection) {
+    std::cout << "Please choose one of the following options:" << std::endl
+        << "Sign In (1), Register (2), Guest (3)" << std::endl;
+    std::cin >> sign_in_selection;
+    switch (sign_in_selection) {
+    case 1 :
+        customerLogin(customerName);
+        break;
+    case 2 :
+        customerRegister(customerName);
+        break;
+    case 3 :
+        greetCustomer(customerName);
+        break;
+    }
+}
+
+void customerLogin(std::string& customerName) {
+    std::cout << "Login" << std::endl;
+};
+
+void customerRegister(std::string& customerName) {
+    std::cout << "Register" << std::endl;
+};
+
+void greetCustomer(std::string customerName) {
+    std::cout << "Welcome to Gurinder's Gurocer, " << customerName << std::endl;
 };
 
 void displayGrocery() {
